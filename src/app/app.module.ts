701410ca89e6a50fd1bcfdfe1c2519b46ff4e745
Oleksandr Poliukh <alexpolyuh95@gmail.com>
2024-08-08 13:56:32 +0100

    push to gitHub

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PasswordStrengthComponent } from './components/password-strength/password-strength.component';
+import { HeaderComponent } from './components/header/header.component';
+import { DescriptionComponent } from './components/description/description.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PasswordStrengthComponent,
+    HeaderComponent,
+    DescriptionComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/password-strength/password-strength.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-password-strength',
+  templateUrl: './password-strength.component.html',
+  styleUrl: './password-strength.component.css'
+})
+export class PasswordStrengthComponent {
+  password: string = "";
+  strengthClass: string[] = ["gray", "gray", "gray"];
+
+  onPasswordInput() {
+    this.checkPassStrength();
+  }
+
+  checkPassStrength() {
+    if (this.password.length === 0) {
+      this.strengthClass = ["gray", "gray", "gray"];
+    } else if (this.password.length < 8) {
+      this.strengthClass = ["red", "red", "red"];
+    } else {
+      const hasLetters = /[a-zA-Z]/.test(this.password);
+      const hasDigits = /\d/.test(this.password);
+      const hasSymbols = /[!@#$%^&*(),.?":{}|<>]/.test(this.password);
+
+      const easyPass = (
+        (hasLetters && !hasDigits && !hasSymbols) ||
+        (!hasLetters && hasDigits && !hasSymbols) ||
+        (!hasLetters && !hasDigits && hasSymbols)
+      );
+
+      const mediumPass = (
+        (hasLetters && !hasDigits && hasSymbols) ||
+        (hasLetters && hasDigits && !hasSymbols) ||
+        (!hasLetters && hasDigits && hasSymbols)
+      );
+
+      const strongPass = hasLetters && hasDigits && hasSymbols;
+
+      if (easyPass) {
+        this.strengthClass = ['red', 'gray', 'gray'];
+      } else if (mediumPass) {
+        this.strengthClass = ['yellow', 'yellow', 'gray'];
+      } else if (strongPass) {
+        this.strengthClass = ['green', 'green', 'green'];
+      }
+    }
+  }
+}
