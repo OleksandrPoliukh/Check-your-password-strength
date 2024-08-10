e8a2db3bf8297cf2389ef8bc94288a06fb99dff3
Oleksandr Poliukh <alexpolyuh95@gmail.com>
2024-08-10 15:05:52 +0100

    Add directive *ngFor to avoid repeating code

--- src/app/components/password-strength-indicator/password-strength-indicator.component.ts
+++ src/app/components/password-strength-indicator/password-strength-indicator.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { NgClass } from '@angular/common';
+import { NgClass, NgForOf } from '@angular/common';
 import { CheckPassStrengthService } from '../../services/check-pass-strength.service';
 
 @Component({
   selector: 'app-password-strength-indicator',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, NgForOf],
   templateUrl: './password-strength-indicator.component.html',
   styleUrls: ['./password-strength-indicator.component.css']
 })
