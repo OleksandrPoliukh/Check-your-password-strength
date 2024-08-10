1c4e3bf991c6cc49ded7599f7f33cf9509a1cf53
Oleksandr Poliukh <alexpolyuh95@gmail.com>
2024-08-10 14:50:52 +0100

    Add ControlValueAccessor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,30 @@ import { DescriptionComponent } from './components/description/description.compo
 import { HeaderComponent } from './components/header/header.component';
 import { PasswordInputComponent } from './components/password-input/password-input.component';
 import { PasswordStrengthIndicatorComponent } from './components/password-strength-indicator/password-strength-indicator.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
     standalone: true,
-    imports: [HeaderComponent, DescriptionComponent, PasswordInputComponent, PasswordStrengthIndicatorComponent]
+    imports: [
+      ReactiveFormsModule,
+      HeaderComponent,
+      DescriptionComponent,
+      PasswordInputComponent,
+      PasswordStrengthIndicatorComponent
+    ]
 })
+
 export class AppComponent {
   title = 'pass-task';
+
+  formGroup: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.formGroup = this.fb.group({
+      password: ['']
+    });
+  }
 }
--- src/app/components/password-input/password-input.component.ts
+++ src/app/components/password-input/password-input.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormControl } from '@angular/forms';
+import { Component, forwardRef } from '@angular/core';
+import { ReactiveFormsModule, FormControl, ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { CheckPassStrengthService } from '../../services/check-pass-strength.service';
 
 @Component({
@@ -7,16 +7,39 @@ import { CheckPassStrengthService } from '../../services/check-pass-strength.ser
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './password-input.component.html',
-  styleUrls: ['./password-input.component.css']
+  styleUrls: ['./password-input.component.css'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => PasswordInputComponent),
+    multi: true
+  }]
 })
 
-export class PasswordInputComponent {
+export class PasswordInputComponent implements ControlValueAccessor {
 
   password = new FormControl('');
 
+  onChange: (value: string) => void = () => {};
+  onTouch: () => void = () => {};
+
   constructor(private checkPassStrengthService: CheckPassStrengthService) {
     this.password.valueChanges.subscribe(value => {
-      this.checkPassStrengthService.checkPassStrength(value || "");
+      if (this.onChange) {
+        this.onChange(value || "");
+      }
+      this.checkPassStrengthService.checkPassStrength((value || ""));
     });
+  }
+
+  writeValue(value: string | null): void {
+    this.password.setValue(value || '');
+  }
+
+  registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouch = fn;
   }
 }
